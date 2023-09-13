e28b5e9368765fc50a9d2c461c31eae12a6ff1c3
urielbang <uri32655@gmail.com>
2023-09-13 10:44:12 +0300

    switchCase

---
+++ main.js
@@ -0,0 +1,18 @@
+function switchTask() {
+  let num = +prompt("type number");
+  switch (num) {
+    case 3:
+      console.log("hello");
+      break;
+    case 5:
+      console.log("goodby");
+      break;
+    case 7:
+      console.log("thanks");
+      break;
+    default:
+      console.log("no messages");
+  }
+}
+
+sdvsdvsdvsdv;
